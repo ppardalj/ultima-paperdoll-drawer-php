6703bf74f1c34913a56705f1763430ae25fe68d1
Pedro Pardal <ppardal@MacBook-Air-de-Pedro.local>
2017-10-12 17:36:37 +0200

    Add Layer class with all the possible layers of items

---
+++ src/Domain/Layer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Ultima\Domain;
+
+class Layer
+{
+    const INVALID = 0;
+    const ONE_HANDED = 1;
+    const TWO_HANDED = 2;
+    const SHOES = 3;
+    const PANTS = 4;
+    const SHIRT = 5;
+    const HELM = 6;
+    const GLOVES = 7;
+    const RING = 8;
+    const TALISMAN = 9;
+    const NECK = 10;
+    const HAIR = 11;
+    const WAIST = 12;
+    const INNER_TORSO = 13;
+    const BRACELET = 14;
+    const FACIAL_HAIR = 16;
+    const MIDDLE_TORSO = 17;
+    const EARRINGS = 18;
+    const ARMS = 19;
+    const CLOAK = 20;
+    const BACKPACK = 21;
+    const OUTER_TORSO = 22;
+    const OUTER_LEGS = 23;
+    const INNER_LEGS = 24;
+}
